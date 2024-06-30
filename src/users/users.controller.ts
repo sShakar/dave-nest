3d1c7d5b924d5207434d2cc3aa98984766cfb1c5
sShakar <shakarkosar01@gmail.com>
2024-07-01 01:20:24 +0300

    added requests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // GET /users or /users?role=value
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  // GET /users/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /users
+  @Post()
+  create(@Body() user: object) {
+    return user;
+  }
+
+  // PATCH /users/:id
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  // DELETE /users/:id
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
